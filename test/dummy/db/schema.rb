966661b3b7f2741c4a68d1bc3edfc58122807804
Jarrod Carlson <jarrod.carlson@gmail.com>
2011-11-08 15:00:14 -0500

    Scaffolded User model in dummy app

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111108195813) do
+ActiveRecord::Schema.define(:version => 20111108195947) do
 
   create_table "permalinks", :force => true do |t|
     t.string   "slug"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(:version => 20111108195813) do
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
