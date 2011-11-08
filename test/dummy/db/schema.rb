594033210117d77453404cbfe3c8aa1f9da11228
Jarrod Carlson <jarrod.carlson@gmail.com>
2011-11-08 14:59:06 -0500

    Scaffolded Post model in dummy app

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111108194929) do
+ActiveRecord::Schema.define(:version => 20111108195813) do
 
   create_table "permalinks", :force => true do |t|
     t.string   "slug"
     t.integer  "content_id"
     t.string   "content_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
