41ec22da07ab93056e325d4d6d94234be70ab09c
Jarrod Carlson <jarrod.carlson@gmail.com>
2011-11-16 20:06:22 -0500

    Move comments

--- lib/slug/filter.rb
+++ lib/slug/filter.rb
@@ -1,11 +1,12 @@
-# This middleware is necessary so that the engine can abstain from processing
-# requests where no such slug exists. If this behavior were done in a
-# controller, Rails would heavily mutate the request object and no additional
-# controllers would have much luck processing the request. Specifically, the
-# named parameters for the route are cached on the request, and downstream
-# controllers would get the wrong params hash.
-
 module Slug
+
+  # This middleware is necessary so that the engine can abstain from processing
+  # requests where no such slug exists. If this behavior were done in a
+  # controller, Rails would heavily mutate the request object and no additional
+  # controllers would have much luck processing the request. Specifically, the
+  # named parameters for the route are cached on the request, and downstream
+  # controllers would get the wrong params hash.
+
   class Filter
     
     def initialize(app)
@@ -15,8 +16,10 @@ module Slug
     def call(env)
       # if a matching slug exists in the database, allow engine to process it
       if Permalink.where('slug = ?', slug(env)).exists?
+        # slug found, allow request to continue to controller
         @app.call(env)
-      else # force engine to abstain from processing this request
+      else
+        # no slug matched, abstain from handling request
         [404, {"X-Cascade" => "pass"}, ["no such slug exists"]]
       end
     end
@@ -27,5 +30,6 @@ module Slug
       slug = slug.slice(1..-1) if slug[0] == 47 # has a leading '/'
       slug
     end
+    
   end
 end
